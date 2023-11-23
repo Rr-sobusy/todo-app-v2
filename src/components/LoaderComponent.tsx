59a1f414ff39f0241bd81f0f828363a145bc0db6
unknown <rockph0029@gmail.com>
2023-11-23 15:56:32 +0800

    final commit

---
+++ src/components/LoaderComponent.tsx
@@ -0,0 +1,16 @@
+
+import "../loader.css";
+
+const LoaderComponent = () => {
+  return (
+    <section className="dots-container">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </section>
+  );
+};
+
+export default LoaderComponent;
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,18 @@
-import React,{Suspense, lazy} from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import "./main.css";
 import { ContextProvider } from "./context/Provider.tsx";
 import { ModalContextProvider } from "./context/ModalContext.tsx";
+import LoaderComponent from "./components/LoaderComponent.tsx";
 
-const DynamicApp = lazy(()=>import('./App.tsx'))
+const DynamicApp = lazy(() => import("./App.tsx"));
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ContextProvider>
       <ModalContextProvider>
-        <Suspense fallback={<p>Loading.....</p>}>
-           <DynamicApp />
+        <Suspense fallback={<LoaderComponent />}>
+          <DynamicApp />
         </Suspense>
       </ModalContextProvider>
     </ContextProvider>
