2c66945392515c5301332a7b3d8eef84b15476a1
unknown <rockph0029@gmail.com>
2023-11-23 15:36:52 +0800

    dynamic

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React,{Suspense, lazy} from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./main.css";
 import { ContextProvider } from "./context/Provider.tsx";
 import { ModalContextProvider } from "./context/ModalContext.tsx";
+
+const DynamicApp = lazy(()=>import('./App.tsx'))
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ContextProvider>
       <ModalContextProvider>
-        <App />
+        <Suspense fallback={<p>Loading.....</p>}>
+           <DynamicApp />
+        </Suspense>
       </ModalContextProvider>
     </ContextProvider>
   </React.StrictMode>
